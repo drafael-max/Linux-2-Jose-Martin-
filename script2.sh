c7e660c499faf5a6deaec58cdd853b365eab9285
drafael-max <drafaelpatin@gmail.com>
2025-12-04 13:19:46 +0000

    sh

---
+++ script2.sh
@@ -0,0 +1,11 @@
+# Mostrar información detallada del sistema operativo y del kernel
+uname -a
+
+# Mostrar información detallada del CPU (arquitectura, núcleos, hilos, velocidad, etc.)
+lscpu
+
+# Mostrar el uso de la memoria RAM en un formato legible (human-readable)
+free -h
+
+# Mostrar el uso del disco (espacio usado y disponible) en formato legible
+df -h
