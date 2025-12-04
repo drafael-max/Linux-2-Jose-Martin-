de70bfcfc3d36c3dc80f3fb7816c749b4c200a52
drafael-max <drafaelpatin@gmail.com>
2025-12-04 12:56:24 +0000

    Mi script

---
+++ script.sh
@@ -0,0 +1,14 @@
+# Crear y escribir texto dentro de un archivo llamado "archivo.txt"
+echo "Hola Linux" > archivo.txt
+
+# Mostrar el contenido del archivo "archivo.txt" en la terminal
+cat archivo.txt
+
+# Copiar el archivo "archivo.txt" a un nuevo archivo llamado "copia.txt"
+cp archivo.txt copia.txt
+
+# Listar los archivos del directorio actual, uno por línea
+ls -l
+
+# Eliminar (borrar) el archivo "archivo.txt"
+rm archivo.txt
